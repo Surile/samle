16bd69221fa20acfcba4782458fe88502c3bb893
Surile <mhecy98@gmail.com>
2019-07-14 01:10:26 +0800

    修改文档

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # samle
-垃圾分类工具后台
 
+> 垃圾分类工具后台
+
+### 功能
+
+- 存储openid
+- 分类展示接口
+- 分类详情接口
+- 获取百度token
+- AI识别- 
+- 搜索接口
+
+### 接口
+
+存储openid `/login`
+
+分类展示接口 `/type`
+
+分类详情接口 `/type/:id`
+
+获取百度token `/image`
+
+AI识别 `/image/serach`
+
+搜索接口 `/goods/search`
+
+
+### 配置文件
+
+修改`config/index.ts`中微信小程序和百度的`ApiKey`和`AppSecret`
+
+
+### 运行
+
+```javascript
+    npm install || npm run start
+    yarn install || yarn start
+```
+
+### 浏览器打开
+
+```javascipt
+    http://localhost:3000
+```
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,14 @@
-export const AppId = 'wx0aff2856cea0f6f8';
-export const AppSecret = 'ce00b32011bf886c2aad3bd7849c6548';
+// 微信小程序
+export const AppId = '';
+export const AppSecret = '';
 export const WxUrl = (code: string) => {
   return `https://api.weixin.qq.com/sns/jscode2session?appid=${AppId}&secret=${AppSecret}&js_code=${code}&grant_type=authorization_code`;
 };
 
 // 百度Ai
 
-export const BaiDuApiKey = 'Pbl6GyPgyWQa6bfIH2rfdkFi';
-export const BaiDuAppSecret = 'VIOqGmsOobQORXdoL2KVf87F8yYCmZxX';
+export const BaiDuApiKey = '';
+export const BaiDuAppSecret = '';
 const BaiDuHostName = 'https://aip.baidubce.com/';
 const BaiDuPath = 'oauth/2.0/token';
 export const BaiDuUrl = `${BaiDuHostName}${BaiDuPath}?grant_type=client_credentials&client_id=${BaiDuApiKey}&client_secret=${BaiDuAppSecret}`;
